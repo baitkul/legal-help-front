bd7864c47f70a38242596fc02249aa34b381c970
Tair <50920351+tairkhan@users.noreply.github.com>
2020-06-22 00:36:13 +0600

    add navbar

---
+++ components/TheNavbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-navbar v-if="isLoggedIn" type="is-primary" shadow>
+    <template #brand>
+      <b-navbar-item tag="nuxt-link" to="/">СЮП</b-navbar-item>
+    </template>
+
+    <template #start>
+      <b-navbar-item tag="nuxt-link" to="/appeals">Обращения</b-navbar-item>
+      <b-navbar-item v-if="!isClient" tag="nuxt-link" to="/users">Пользователи</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/settings">Настройки</b-navbar-item>
+    </template>
+
+    <template #end>
+      <b-navbar-dropdown :label="getUser.fullname">
+        <b-navbar-item tag="button" @click="$auth.logout()">Выйти</b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'isLoggedIn',
+      'isClient',
+      'getUser',
+    ])
+  },
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,21 @@
 <template>
-  <div class="min-h-screen flex">
-    <div
-      v-if="isLoggedIn"
-      class="p-1 w-16 lg:w-56 h-full fixed flex flex-col bg-gray-700 text-white"
-    >
-      <div class="p-3 bg-gray-900 flex flex-col items-center text-center lg:text-left cursor-default rounded">
-        <fa-icon class="fa-fw lg:mr-1" icon="user-circle" size="2x"></fa-icon>
-        <div class="mt-2 hidden lg:block font-bold text-xs text-center">{{ getUser.fullname }}</div>
-      </div>
-
-      <nuxt-link class="mt-2 p-3 hover:bg-gray-900 text-center lg:text-left rounded" to="/appeals">
-        <fa-icon class="fa-fw lg:mr-1" icon="file-alt" size="lg"></fa-icon>
-        <span class="hidden lg:inline-block">Обращения</span>
-      </nuxt-link>
-
-      <nuxt-link v-if="!isClient" class="mt-2 p-3 hover:bg-gray-900 text-center lg:text-left rounded" to="/users">
-        <fa-icon class="fa-fw lg:mr-1" icon="user-friends" size="lg"></fa-icon>
-        <span class="hidden lg:inline-block">Пользователи</span>
-      </nuxt-link>
-
-      <nuxt-link class="mt-2 p-3 hover:bg-gray-900 text-center lg:text-left rounded" to="/settings">
-        <fa-icon class="fa-fw lg:mr-1" icon="cog" size="lg"></fa-icon>
-        <span class="hidden lg:inline-block">Настройки</span>
-      </nuxt-link>
-
-      <button class="mt-2 p-3 hover:bg-gray-900 text-center lg:text-left rounded" @click="$auth.logout()">
-        <fa-icon class="fa-fw lg:mr-1" icon="sign-out-alt" size="lg"></fa-icon>
-        <span class="hidden lg:inline-block">Выйти</span>
-      </button>
-    </div>
-    <nuxt class="ml-16 lg:ml-56 flex-1 bg-gray-100 p-3" />
+  <div class="min-h-screen flex flex-col">
+    <TheNavbar />
+    <nuxt class="flex-1 bg-gray-100 p-3" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import TheNavbar from '~/components/TheNavbar'
 
 export default {
   middleware: 'auth',
-  computed: {
-    ...mapGetters([
-      'isLoggedIn',
-      'isClient',
-      'getUser',
-    ])
+  components: {
+    TheNavbar,
   },
 }
 </script>
 
 <style>
-a:hover,
-.nuxt-link-exact-active {
-  @apply text-yellow-500;
-}
+
 </style>
